4c300042184021ba965874d80d7731d0ea2fd6ac
savio alexandre <alexandresaviosouza@gmail.com>
2026-08-10 16:02:28 -0300

    atualiza arquivos

--- QAx/playwright-mark/tests/tasks.spec.ts
+++ QAx/playwright-mark/tests/tasks.spec.ts
@@ -1,22 +1,49 @@
 import { test, expect } from '@playwright/test';
+interface TaskModel {
+  name: string
+  is_done: boolean
+}
 
 test('deve poder cadastrar uma nova tarefa (validando comportamento )', async ({ page, request }) => {
-
-  const taskName = 'Ler um livro de TypeScript';
-
-  await request.delete('http://localhost:3333/helper/tasks/' + taskName);
+  const task: TaskModel = {
+    name: 'Ler um livro de TypeScript',
+    is_done: false
+  }
+  await request.delete('http://localhost:3333/helper/tasks/' + task.name);
 
   await page.goto('http://localhost:8080/');
 
   const inputTaskName = page.locator('input[class*=InputNewTask]');
-  await inputTaskName.fill(taskName);
-  await page.click('xpath=//button[contains(text(), "Create")]');
+  await inputTaskName.fill(task.name);
+  await page.click('css=button >> text=Create');
 
-  const target = page.locator(`css=.task-item p >> text=${taskName}`);
-  await expect(target).toBeVisible();  
+  const target = page.locator(`css=.task-item p >> text=${task.name}`);
+  await expect(target).toBeVisible();
 
 });
 
-test('não deve permitir tarefa duplicada', async ({page}) => {
-  
+test.only('não deve permitir tarefa duplicada', async ({ page, request }) => {
+
+  const task: TaskModel = {
+    name: 'Estudar algoritmos',
+    is_done: false
+  };
+
+  await request.delete('http://localhost:3333/helper/tasks/' + task.name);
+
+  const newTask = await request.post('http://localhost:3333/tasks/', { data: task });
+  expect(newTask.ok()).toBeTruthy();
+
+  await page.goto('http://localhost:8080/');
+
+  const inputTaskName = page.locator('input[class*=InputNewTask]');
+  await inputTaskName.fill(task.name);
+
+  await page.click('css=button >> text=Create');
+
+  const target = page.locator('.swal2-html-container');
+  await expect(target).toHaveText('Task already exists!');
+
+
+
 });
